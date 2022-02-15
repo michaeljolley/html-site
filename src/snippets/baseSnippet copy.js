776653e87cea19038586d6945edab627dff4102e
Michael Jolley <mike@sparcapp.io>
2022-02-15 17:58:05 -0600

    Adding css

--- src/snippets/baseSnippet copy.js
+++
@@ -1,7 +0,0 @@
-import Handlebars from 'handlebars';
-
-export default function baseHelper(html, data) {
-  const template = Handlebars.compile(html);
-  const renderedContent = template(data);
-  return new Handlebars.SafeString(renderedContent);
-}
