dca0ecc0fb3123cc6efa0ee3da51abb5dd9a7a5f
Michael Jolley <mike@sparcapp.io>
2022-02-21 17:47:08 -0600

    Adding chutney

--- build.js
+++ build.js
@@ -1,16 +1,22 @@
-import moveStaticFiles from './chutney/moveStaticFiles.js';
-import registerPages from './chutney/registerPages.js';
-import registerHelpers from './chutney/registerHelpers.js';
-import gatherData from './chutney/gatherData.js';
+// import moveStaticFiles from './chutney/moveStaticFiles.js';
+// import registerPages from './chutney/registerPages.js';
+// import registerHelpers from './chutney/registerHelpers.js';
+// import gatherData from './chutney/gatherData.js';
 
-async function main() {
+// async function main() {
 
-  // Get data to be used across the entire site.
-  const data = await gatherData();
+//   // Get data to be used across the entire site.
+//   const data = await gatherData();
 
-  await registerHelpers();
-  await registerPages(data);
-  await moveStaticFiles();
-}
+//   await registerHelpers();
+//   await registerPages(data);
+//   await moveStaticFiles();
+// }
 
-main();
+// main();
+
+
+import chutney from 'chutney';
+
+
+chutney();
